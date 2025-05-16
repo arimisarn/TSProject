fb6cf95db8f8f5316e9486a27eeafb716154b9df
R-code <rakotomalalacode@gmail.com>
2025-05-16 13:20:47 +0300

    premiere page - connexion - innscription

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,58 @@
 "use strict";
-const message = "Bonjour depuis TypeScript !";
-console.log(message);
-const monElement = document.getElementById("monElement");
-if (monElement) {
-    monElement.textContent = message;
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function loadHeader() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch("../components/headers.html");
+            const data = yield response.text();
+            const headerElement = document.getElementById("header");
+            if (headerElement) {
+                headerElement.innerHTML = data;
+            }
+        }
+        catch (error) {
+            console.error('erreur lors du chargement');
+        }
+    });
 }
+loadHeader();
+function loadMain() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch("../components/mainIndex.html");
+            const data = yield response.text();
+            const headerElement = document.getElementById("main");
+            if (headerElement) {
+                headerElement.innerHTML = data;
+            }
+        }
+        catch (error) {
+            console.error('erreur lors du chargement');
+        }
+    });
+}
+loadMain();
+function loadFooter() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch("../components/footer.html");
+            const data = yield response.text();
+            const headerElement = document.getElementById("footer");
+            if (headerElement) {
+                headerElement.innerHTML = data;
+            }
+        }
+        catch (error) {
+            console.error('erreur lors du chargement');
+        }
+    });
+}
+loadFooter();
---
+++ ts/main.ts
@@ -0,0 +1,41 @@
+async function loadHeader() {
+    try{
+        const response = await fetch ("../components/headers.html")
+        const data =await response.text()
+        const headerElement = document.getElementById("header")
+        if(headerElement){
+            headerElement.innerHTML = data
+        }
+    } catch(error){
+        console.error('erreur lors du chargement')
+    }
+}
+loadHeader()
+
+async function loadMain() {
+    try{
+        const response = await fetch ("../components/mainIndex.html")
+        const data =await response.text()
+        const headerElement = document.getElementById("main")
+        if(headerElement){
+            headerElement.innerHTML = data
+        }
+    } catch(error){
+        console.error('erreur lors du chargement')
+    }
+}
+loadMain()
+
+async function loadFooter() {
+    try{
+        const response = await fetch ("../components/footer.html")
+        const data =await response.text()
+        const headerElement = document.getElementById("footer")
+        if(headerElement){
+            headerElement.innerHTML = data
+        }
+    } catch(error){
+        console.error('erreur lors du chargement')
+    }
+}
+loadFooter()
